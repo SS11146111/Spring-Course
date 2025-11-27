6318210f87473c7075f6d20f1576ae9b54565dd5
SS11146111 <sahasangita59@gmail.com>
2025-11-27 13:49:29 +0530

    twelfth commit

---
+++ example12/src/main/java/org/sangita/beans/Person.java
@@ -0,0 +1,33 @@
+package org.sangita.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    public Person(){
+        System.out.println("Person bean created by Spring");
+    }
+
+    private String name = "Lucy";
+
+    @Autowired(required = false)
+    private Vehicle vehicle;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+}
