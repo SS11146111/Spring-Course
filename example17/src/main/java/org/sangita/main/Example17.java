94d1f529e720db4ceadd673ab27e374b9c2a455d
SS11146111 <sahasangita59@gmail.com>
2025-11-29 11:28:46 +0530

    autowiring using @Qualifier

---
+++ example17/src/main/java/org/sangita/main/Example17.java
@@ -0,0 +1,18 @@
+package org.sangita.main;
+
+import org.sangita.beans.Person;
+import org.sangita.beans.Vehicle;
+import org.sangita.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example17 {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Person person = context.getBean(Person.class);
+        System.out.println("Person name from Spring Context is: " + person.getName());
+        System.out.println("Vehicle that Person own is: " + person.getVehicle());
+
+    }
+}
