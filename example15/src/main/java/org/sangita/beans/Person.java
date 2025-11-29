e1f005ee3ac5020350090d3b05b4fe33100ac50f
SS11146111 <sahasangita59@gmail.com>
2025-11-29 11:05:05 +0530

    autowiring using constructor parameter name

---
+++ example15/src/main/java/org/sangita/beans/Person.java
@@ -0,0 +1,31 @@
+package org.sangita.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    private String name="Lucy";
+    private final Vehicle vehicle;
+
+    @Autowired
+    public Person(Vehicle vehicle1){
+        System.out.println("Person bean created by Spring");
+        this.vehicle = vehicle1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+}
