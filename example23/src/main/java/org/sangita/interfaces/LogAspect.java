7be950d61e6af2f0f84f4b320e7d30c0b38740a6
SS11146111 <sahasangita59@gmail.com>
2025-12-08 14:27:20 +0530

    problem without AOP

---
+++ example23/src/main/java/org/sangita/interfaces/LogAspect.java
@@ -0,0 +1,11 @@
+package org.sangita.interfaces;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface LogAspect {
+}
---
+++ example23/src/main/java/org/sangita/main/Example23.java
@@ -0,0 +1,22 @@
+package org.sangita.main;
+
+import org.sangita.config.ProjectConfig;
+import org.sangita.model.Song;
+import org.sangita.services.VehicleServices;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example23{
+
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var vehicleServices = context.getBean(VehicleServices.class);
+        System.out.println(vehicleServices.getClass());
+        Song song = new Song();
+        song.setTitle("Blank Space");
+        song.setSingerName("Taylor Swift");
+        boolean vehicleStarted = true;
+        String moveVehicleStatus = vehicleServices.moveVehicle(vehicleStarted);
+        String playMusicStatus = vehicleServices.playMusic(vehicleStarted,song);
+        String applyBrakeStatus = vehicleServices.applyBrake(vehicleStarted);
+    }
+}
