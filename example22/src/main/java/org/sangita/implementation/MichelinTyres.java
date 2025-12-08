3153a19c83ff65ef07a7b438c58c444c46b40025
SS11146111 <sahasangita59@gmail.com>
2025-12-08 14:25:31 +0530

    prototype scoped bean demo

---
+++ example22/src/main/java/org/sangita/implementation/MichelinTyres.java
@@ -0,0 +1,15 @@
+package org.sangita.implementation;
+
+import org.sangita.interfaces.Tyres;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class MichelinTyres implements Tyres {
+
+    public String rotate(){
+        return "Vehicle moving with Michelin tyres";
+    }
+
+}
