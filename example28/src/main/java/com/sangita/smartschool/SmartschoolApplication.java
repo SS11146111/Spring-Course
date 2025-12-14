1d680d48328fa7079f1bac66d1d67035ee2dd53b
SS11146111 <sahasangita59@gmail.com>
2025-12-14 15:46:09 +0530

    Spring boot based web application exclusion list

--- example28/src/main/java/com/sangita/smartschool/SmartschoolApplication.java
+++ example28/src/main/java/com/sangita/smartschool/SmartschoolApplication.java
@@ -2,8 +2,9 @@ package com.sangita.smartschool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class SmartschoolApplication {
 
 	public static void main(String[] args) {
