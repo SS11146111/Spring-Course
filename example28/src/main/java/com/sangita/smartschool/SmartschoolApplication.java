60b993090b79573468d4d89f73e46ba74612dbc4
SS11146111 <sahasangita59@gmail.com>
2025-12-15 13:20:45 +0530

    Spring boot based web application setting default home page

--- example28/src/main/java/com/sangita/smartschool/SmartschoolApplication.java
+++ example28/src/main/java/com/sangita/smartschool/SmartschoolApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+//@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication
 public class SmartschoolApplication {
 
 	public static void main(String[] args) {
