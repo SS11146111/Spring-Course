e38494bb25f52837c0315171a60d91845ad14bce
SS11146111 <sahasangita59@gmail.com>
2025-12-21 13:18:48 +0530

    Spring boot based web application submit information from contact page and log into console in backend using pojo class

---
+++ example28/src/main/java/com/sangita/smartschool/service/ContactService.java
@@ -0,0 +1,34 @@
+package com.sangita.smartschool.service;
+
+import com.sangita.smartschool.controller.ContactController;
+import com.sangita.smartschool.model.Contact;
+//import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/*
+@Slf4j, is a Lombok-provided annotation that will automatically generate an SLF4J
+Logger static property in the class at compilation time.
+* */
+//@Slf4j
+
+@Service
+public class ContactService {
+
+    /**
+     * Save Contact Details into DB
+     * @param contact
+     * @return boolean
+     */
+
+    private static Logger log = LoggerFactory.getLogger(ContactService.class);
+
+    public boolean saveMessageDetails(Contact contact){
+        boolean isSaved = true;
+        //TODO - Need to persist the data into the DB table
+        log.info(contact.toString());
+        return isSaved;
+    }
+
+}
