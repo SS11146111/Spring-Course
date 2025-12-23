b04e77a671e6e94e7221d28b1132ddd040dec999
SS11146111 <sahasangita59@gmail.com>
2025-12-23 15:31:08 +0530

    Spring boot based web application Lombok integration

--- example28/src/main/java/com/sangita/smartschool/model/Contact.java
+++ example28/src/main/java/com/sangita/smartschool/model/Contact.java
@@ -1,13 +1,13 @@
 package com.sangita.smartschool.model;
 
-//import lombok.Data;
+import lombok.Data;
 
 /*
 @Data annotation is provided by Lombok library which generates getter, setter,
 equals(), hashCode(), toString() methods & Constructor at compile time.
 This makes our code short and clean.
 **/
-/*@Data
+@Data
 public class Contact {
 
     private String name;
@@ -15,65 +15,6 @@ public class Contact {
     private String email;
     private String subject;
     private String message;
-}*/
-
-
-public class Contact{
-    private String name;
-    private String mobileNum;
-    private String email;
-    private String subject;
-    private String message;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMobileNum() {
-        return mobileNum;
-    }
-
-    public void setMobileNum(String mobileNum) {
-        this.mobileNum = mobileNum;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "name='" + name + '\'' +
-                ", mobileNum='" + mobileNum + '\'' +
-                ", email='" + email + '\'' +
-                ", subject='" + subject + '\'' +
-                ", message='" + message + '\'' +
-                '}';
-    }
 }
+
 
--- example28/src/main/java/com/sangita/smartschool/model/Holiday.java
+++ example28/src/main/java/com/sangita/smartschool/model/Holiday.java
@@ -1,8 +1,8 @@
 package com.sangita.smartschool.model;
 
-//import lombok.Data;
+import lombok.Data;
 
-/*@Data
+@Data
 public class Holiday {
 
     private final String day;
@@ -11,36 +11,7 @@ public class Holiday {
 
     public enum Type {
         FESTIVAL, FEDERAL
-    }
-}*/
-
-public class Holiday {
-
-    private final String day;
-    private final String reason;
-    private final Type type;
-
-    public enum Type {
-        FESTIVAL, FEDERAL
-    }
-
-    public Holiday(String day, String reason, Type type)
-    {
-        this.day = day;
-        this.reason = reason;
-        this.type = type;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public Type getType() {
-        return type;
     }
 }
+
 
