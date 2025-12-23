081a4a5963527eb0a0d543a542dac60b4fbba04c
SS11146111 <sahasangita59@gmail.com>
2025-12-23 15:52:53 +0530

    Spring boot based web application Lombok library @Slf4j

--- example28/src/main/java/com/sangita/smartschool/service/ContactService.java
+++ example28/src/main/java/com/sangita/smartschool/service/ContactService.java
@@ -3,6 +3,7 @@ package com.sangita.smartschool.service;
 import com.sangita.smartschool.controller.ContactController;
 import com.sangita.smartschool.model.Contact;
 //import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,8 @@ import org.springframework.stereotype.Service;
 @Slf4j, is a Lombok-provided annotation that will automatically generate an SLF4J
 Logger static property in the class at compilation time.
 * */
-//@Slf4j
 
+@Slf4j
 @Service
 public class ContactService {
 
@@ -22,7 +23,7 @@ public class ContactService {
      * @return boolean
      */
 
-    private static Logger log = LoggerFactory.getLogger(ContactService.class);
+    //private static Logger log = LoggerFactory.getLogger(ContactService.class);
 
     public boolean saveMessageDetails(Contact contact){
         boolean isSaved = true;
