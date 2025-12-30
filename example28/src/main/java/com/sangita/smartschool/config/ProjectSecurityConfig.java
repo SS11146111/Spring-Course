c1d540554a5151f81c0d5351baa7932120e7a94d
SS11146111 <sahasangita59@gmail.com>
2025-12-30 16:44:58 +0530

    Spring boot based web application denyAll() requests demo

--- example28/src/main/java/com/sangita/smartschool/config/ProjectSecurityConfig.java
+++ example28/src/main/java/com/sangita/smartschool/config/ProjectSecurityConfig.java
@@ -12,15 +12,15 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        // Permit All Requests inside the Web Application
+       /* // Permit All Requests inside the Web Application
         http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
                 .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults());*/
 
         // Deny All Requests inside the Web Application
-            /*http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
+            http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
                 .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());*/
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
 
