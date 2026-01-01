297ce5dfcc6c7cdc614a6fa88c2df48eb69a3103
SS11146111 <sahasangita59@gmail.com>
2026-01-01 14:58:34 +0530

    Spring boot based web application demo of configuring  multiple users usinf inMemoryAuthentication() of Spring Security

--- example28/src/main/java/com/sangita/smartschool/config/ProjectSecurityConfig.java
+++ example28/src/main/java/com/sangita/smartschool/config/ProjectSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,29 +15,37 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        // Permit All Requests inside the Web Application
-        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(requests ->
-                        requests
-                                .requestMatchers("/","/home").permitAll()
-                                .requestMatchers("/courses").permitAll()
-                                .requestMatchers("/contact").permitAll()
-                                //.requestMatchers("/holidays/**").permitAll()
-                                .requestMatchers("/holidays/**").authenticated()
-                                .requestMatchers("/about").permitAll()
-                                .requestMatchers("/saveMsg").permitAll()
-                                .requestMatchers("/assets/**").permitAll()
-                )
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests
+                //.requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/", "/home").authenticated()
+                .requestMatchers("/holidays/**").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/saveMsg").permitAll()
+                .requestMatchers("/courses").permitAll()
+                .requestMatchers("/about").permitAll()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/assets/**").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
 
-
-        /*// Deny All Requests inside the Web Application
-            http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());*/
-
         return http.build();
+    }
 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("54321")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
