f9a0678d469dc4a86d506cffe16db7376a09253b
SS11146111 <sahasangita59@gmail.com>
2026-01-17 10:26:30 +0530

    Spring boot based web application building admin dashboard complete

---
+++ example28/src/main/java/com/sangita/smartschool/repository/CoursesRepository.java
@@ -0,0 +1,10 @@
+package com.sangita.smartschool.repository;
+
+import com.sangita.smartschool.model.Courses;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoursesRepository extends JpaRepository<Courses, Integer> {
+
+}
