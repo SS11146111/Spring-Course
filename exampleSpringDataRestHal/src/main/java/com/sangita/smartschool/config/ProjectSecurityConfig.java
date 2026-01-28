03d00557a945c35a5fb93ad99298aedfe6442e3f
SS11146111 <sahasangita59@gmail.com>
2026-01-28 10:11:51 +0530

    Spring boot based web application securing spring data rest and hal explorer

--- exampleSpringDataRestHal/src/main/java/com/sangita/smartschool/config/ProjectSecurityConfig.java
+++ exampleSpringDataRestHal/src/main/java/com/sangita/smartschool/config/ProjectSecurityConfig.java
@@ -1,17 +1,11 @@
 package com.sangita.smartschool.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,67 +14,43 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        //http.csrf((csrf) -> csrf.disable())
-        http.csrf((csrf) -> csrf.ignoringRequestMatchers("/saveMsg")/*.ignoringRequestMatchers(PathRequest.toH2Console())*/.ignoringRequestMatchers("/public/**").ignoringRequestMatchers("/api/**"))
-                .authorizeHttpRequests((requests) -> requests
-                                .requestMatchers("/public/**").permitAll()
-                                .requestMatchers("/", "/home").permitAll()
-                                .requestMatchers("/holidays/**").permitAll()
-                                .requestMatchers("/data-api/**").permitAll()
-                                /*.requestMatchers("/courseses/**").permitAll()
-                                .requestMatchers("/contacts/**").permitAll()*/
-                                .requestMatchers("/contact").permitAll()
-                                .requestMatchers("/saveMsg").permitAll()
-                                .requestMatchers("/courses").permitAll()
-                                .requestMatchers("/about").permitAll()
-                                .requestMatchers("/login").permitAll()
-                                .requestMatchers("/assets/**").permitAll()
-                                .requestMatchers("/favicon.ico").permitAll()
-                                .requestMatchers("/logout").permitAll()
-                                // .requestMatchers(PathRequest.toH2Console()).permitAll()
-                                .requestMatchers("/dashboard").authenticated()
-                                .requestMatchers("/api/**").hasRole("ADMIN")
-                                .requestMatchers("/displayMessages/**").hasRole("ADMIN")
-                                .requestMatchers("/admin/**").hasRole("ADMIN")
-                                .requestMatchers("/displayProfile").authenticated()
-                                .requestMatchers("/updateProfile").authenticated()
-                                .requestMatchers("/student/**").hasRole("STUDENT")
-                                .requestMatchers("/closeMsg/**").hasRole("ADMIN")
-
-
-                )
+        http.csrf((csrf) -> csrf.ignoringRequestMatchers("/saveMsg").ignoringRequestMatchers("/public/**")
+                .ignoringRequestMatchers("/api/**").ignoringRequestMatchers("/data-api/**"))
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/dashboard").authenticated()
+                    .requestMatchers("/displayMessages/**").hasRole("ADMIN")
+                    .requestMatchers("/closeMsg/**").hasRole("ADMIN")
+                    .requestMatchers("/admin/**").hasRole("ADMIN")
+                    .requestMatchers("/api/**").authenticated()
+                    .requestMatchers("/data-api/**").authenticated()
+                    .requestMatchers("/displayProfile").authenticated()
+                    .requestMatchers("/updateProfile").authenticated()
+                    .requestMatchers("/student/**").hasRole("STUDENT")
+                    /*.requestMatchers("/profile/**").permitAll()
+                    .requestMatchers("/courseses/**").permitAll()
+                    .requestMatchers("/contacts/**").permitAll()
+                     .requestMatchers("/data-api/**").permitAll()*/
+                    .requestMatchers("/", "/home").permitAll()
+                    .requestMatchers("/holidays/**").permitAll()
+                    .requestMatchers("/contact").permitAll()
+                    .requestMatchers("/saveMsg").permitAll()
+                    .requestMatchers("/courses").permitAll()
+                    .requestMatchers("/about").permitAll()
+                    .requestMatchers("/assets/**").permitAll()
+                    .requestMatchers("/login").permitAll()
+                    .requestMatchers("/logout").permitAll()
+                    .requestMatchers("/public/**").permitAll())
                 .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
                         .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
                 .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
                         .invalidateHttpSession(true).permitAll())
                 .httpBasic(Customizer.withDefaults());
 
-       // http
-         //       .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
         return http.build();
     }
-
-   /* @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("12345")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("54321")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }*/
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 
 }
