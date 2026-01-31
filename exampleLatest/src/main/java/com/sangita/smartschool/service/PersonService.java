61c0a87e0d7b8cf58456a8d8b7ab81adfa4b7ed0
SS11146111 <sahasangita59@gmail.com>
2026-02-01 00:25:33 +0530

    Spring boot web app latest

---
+++ exampleLatest/src/main/java/com/sangita/smartschool/service/PersonService.java
@@ -0,0 +1,36 @@
+package com.sangita.smartschool.service;
+
+import com.sangita.smartschool.constants.SmartSchoolConstants;
+import com.sangita.smartschool.model.Person;
+import com.sangita.smartschool.model.Roles;
+import com.sangita.smartschool.repository.PersonRepository;
+import com.sangita.smartschool.repository.RolesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private RolesRepository rolesRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean createNewPerson(Person person){
+        boolean isSaved = false;
+        Roles role = rolesRepository.getByRoleName(SmartSchoolConstants.STUDENT_ROLE);
+        person.setRoles(role);
+        person.setPwd(passwordEncoder.encode(person.getPwd()));
+        person = personRepository.save(person);
+        if (null != person && person.getPersonId() > 0)
+        {
+            isSaved = true;
+        }
+        return isSaved;
+    }
+}
